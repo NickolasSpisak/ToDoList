3d5bf92c6ab898fd5ac0986c5a563e07f25407d6
spish-web <nickolas.spisak@gmail.com>
2022-01-22 01:47:04 -0500

    First Commit

---
+++ to-do-list/src/componants/mainComponant.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./homeComponant";
+
+class Main extends Component {
+  render() {
+    return <Home />;
+  }
+}
+export default Main;
